10aa93201cd4c242088a9905395c8382bd0d73f6
flechaVivas <facundovivas15@gmail.com>
2022-07-18 14:59:08 -0300

    added atbs

--- src/entities/MovieRoom.java
+++ src/entities/MovieRoom.java
@@ -4,6 +4,8 @@ public class MovieRoom {
 
 	private int roomNumber;
 	private int capacity;
+	private int row;
+	private String col;
 	
 	
 	public int getRoomNumber() {
@@ -18,10 +20,21 @@ public class MovieRoom {
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-	
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public String getCol() {
+		return col;
+	}
+	public void setCol(String col) {
+		this.col = col;
+	}
 	@Override
 	public String toString() {
-		return "MovieRoom [roomNumber=" + roomNumber + ", capacity=" + capacity + "]";
+		return "MovieRoom [roomNumber=" + roomNumber + ", capacity=" + capacity + ", row=" + row + ", col=" + col + "]";
 	}
 	
 }
