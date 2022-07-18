d230b4610de954f2c5de3310d7b9e232a40db5ea
flechaVivas <facundovivas15@gmail.com>
2022-07-18 14:29:38 -0300

    added Genre and MovieRoom

---
+++ src/entities/Genre.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Genre {
+
+	private int idGenre;
+	private String description;
+	
+	public int getIdGenre() {
+		return idGenre;
+	}
+	public void setIdGenre(int idGenre) {
+		this.idGenre = idGenre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Genre() {}
+	
+}
---
+++ src/entities/MovieRoom.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class MovieRoom {
+
+	private int roomNumber;
+	private int capacity;
+	
+	
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	@Override
+	public String toString() {
+		return "MovieRoom [roomNumber=" + roomNumber + ", capacity=" + capacity + "]";
+	}
+	
+}
