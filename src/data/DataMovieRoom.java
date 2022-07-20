fd48a819c0aaf8863aad07eef78d92cbbddc04b6
FacuTaborra <taborrafacundo@gmail.com>
2022-07-20 16:35:11 -0300

    agrego MovieRoomController y dataMovieRoom

--- src/data/DataMovieRoom.java
+++ src/data/DataMovieRoom.java
@@ -38,6 +38,93 @@ public class DataMovieRoom {
 		return mr;
 	}
 	
+	public LinkedList<MovieRoom> list(){
+		Statement stmt = null;
+		ResultSet rs = null;
+		LinkedList<MovieRoom> mrs = new LinkedList<MovieRoom>();
+		try {
+			stmt = DbConnector.getInstancia().getConn().createStatement();
+			rs = stmt.executeQuery("SELECT * FROM movieRoom");
+			while(rs.next()) {
+				MovieRoom mr = new MovieRoom();
+				mr.setRoomNumber(rs.getInt("roomNumber"));
+				mr.setCapacity(rs.getInt("capacity"));
+				mrs.add(mr);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null) {rs.close();}
+				if(stmt!=null) {stmt.close();}
+				DbConnector.getInstancia().releaseConn();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return mrs;
+	}
 	
+	public MovieRoom update(MovieRoom mr) {
+		PreparedStatement stmt = null;
+		try {
+			stmt = DbConnector.getInstancia().getConn().prepareStatement("UPDATE movieroom set capacity = ? where = roomNumber ");			
+			stmt.setInt(1, mr.getCapacity());
+			stmt.setInt(2, mr.getRoomNumber());
+			stmt.executeUpdate();
+		}catch (SQLException e) {
+            e.printStackTrace();
+		} finally {
+            try {
+                if(stmt!=null)stmt.close();
+                DbConnector.getInstancia().releaseConn();
+            } catch (SQLException e) {
+            	e.printStackTrace();
+            }
+		}
+		return mr;
+	}
+	
+	public MovieRoom CreateMovieRoom(MovieRoom mr){
+		PreparedStatement stmt = null;
+		
+		try {
+			stmt = DbConnector.getInstancia().getConn().prepareStatement("INSERT INTO movieroom(capacity) values(?)", PreparedStatement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, mr.getCapacity());
+			stmt.executeUpdate();
+			ResultSet keyResultSet = stmt.getGeneratedKeys();
+			if(keyResultSet != null && keyResultSet.next()) {
+				mr.setRoomNumber(keyResultSet.getInt(1));
+			}
+		}catch (SQLException e) {
+            e.printStackTrace();
+		} finally {
+            try {
+                if(stmt!=null)stmt.close();
+                DbConnector.getInstancia().releaseConn();
+            } catch (SQLException e) {
+            	e.printStackTrace();
+            }
+		}
+		return mr;
+	}
+	
+	public void deleteMovieRoom(MovieRoom mr) {
+		PreparedStatement stmt = null;
+		try {
+			stmt = DbConnector.getInstancia().getConn().prepareStatement("DELETE FROM movieroom WHERE roomNumber ");
+			stmt.setInt(1, mr.getRoomNumber());
+			stmt.executeUpdate();
+		}catch (SQLException e) {
+            e.printStackTrace();
+		} finally {
+            try {
+                if(stmt!=null)stmt.close();
+                DbConnector.getInstancia().releaseConn();
+            } catch (SQLException e) {
+            	e.printStackTrace();
+            }
+		}
+	}
 	
 }
--- src/logic/MovieRoomController.java
+++ src/logic/MovieRoomController.java
@@ -1,7 +1,36 @@
 package logic;
 
+import java.util.LinkedList;
+import data.DataMovieRoom;
+import entities.MovieRoom;
+
 public class MovieRoomController {
 
+	private DataMovieRoom dmr;
 	
+	public MovieRoomController() {
+		dmr = new DataMovieRoom();
+	}
 	
+	public MovieRoom getOne(MovieRoom m) {
+		return dmr.getOne(m);
+	}
+	
+	public LinkedList<MovieRoom> list(){
+		return dmr.list();
+	}
+	
+	public MovieRoom update(MovieRoom mr) {
+		return dmr.update(mr);
+	}
+	
+	public MovieRoom CreateMovieRoom(MovieRoom mr){
+		return dmr.CreateMovieRoom(mr);
+	}
+	
+	public void deleteMovieRoom(MovieRoom mr) {
+		dmr.deleteMovieRoom(mr);
+	}
+
+
 }
