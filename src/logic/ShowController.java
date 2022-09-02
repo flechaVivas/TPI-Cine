467d2ab1130ad1e3f564f70dcc588f9a16546e21
NahuBuschittari <nahubuschittari0310@gmail.com>
2022-09-02 12:05:28 -0300

    Cambio en controlador de Show

--- src/logic/ShowController.java
+++ src/logic/ShowController.java
@@ -1,7 +1,6 @@
 package logic;
 
 import data.DataShow;
-import entities.Movie;
 import entities.Show;
 
 public class ShowController {
@@ -10,8 +9,6 @@ public class ShowController {
 	public ShowController(){ds= new DataShow();}
 	public void getOne(Show s) {ds.getOne(s);}
 	public void deleteOne(Show s) {ds.delete(s);}
-	
-	public Movie editOne(Show s) {return ds.update(s);}
-	
-	public Movie addOne(Show s) {return ds.createOne(s);}
+	public Show editOne(Show s) {return ds.update(s);}
+	public Show addOne(Show s) {return ds.createOne(s);}
 }
