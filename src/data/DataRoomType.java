496446a10fc391db5d4e70ebe0666cf13fa04168
flechaVivas <facundovivas15@gmail.com>
2022-10-14 23:16:44 -0300

    add getAvailableByMovie method (working)

--- src/data/DataRoomType.java
+++ src/data/DataRoomType.java
@@ -3,8 +3,15 @@ package data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.LinkedList;
 
+import entities.Movie;
+import entities.Role;
 import entities.RoomType;
+import entities.User;
+import logic.RoleController;
 
 public class DataRoomType {
 
@@ -15,9 +22,6 @@ public class DataRoomType {
 		try {
 			stmt=DbConnector.getInstancia().getConn().prepareStatement("SELECT * FROM room_type WHERE idRoomType=?");
 			stmt.setInt(1, rt.getIdRoomType());
-			stmt.setString(2, rt.getDescription());
-			stmt.setInt(3, rt.getSizeRow());
-			stmt.setInt(4, rt.getSizeCol());
 			rs=stmt.executeQuery();
 			
 			r.setIdRoomType(rs.getInt("idRoomType"));
@@ -38,7 +42,59 @@ public class DataRoomType {
 		}
 		return r;
 	}
+	
+	public LinkedList<RoomType> getAvailablesByMovie(Movie m){
 
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		LinkedList<RoomType> tipos = new LinkedList<RoomType>();
+		
+		try {
+			stmt=DbConnector.getInstancia().getConn().prepareStatement(
+					"select distinct rt.idRoomType, rt.description, rt.rowQuantity, rt.colQuantity\n" + 
+					"from room_type rt\n" + 
+					"inner join movieroom mr\n" + 
+					"	on mr.idRoomType=rt.idRoomType\n" + 
+					"inner join cine_tpjava.show s\n" + 
+					"	on s.roomNumber=mr.roomNumber\n" + 
+					"inner join movie m\n" + 
+					"	on m.idMovie=s.idMovie\n" + 
+					"where m.idMovie = ?;"
+			);
+			stmt.setInt(1, m.getIdMovie());
+			rs=stmt.executeQuery();
+			
+			if (rs != null) {
+				while (rs.next()) {
+					RoomType rt = new RoomType();
+					
+					rt.setIdRoomType((rs.getInt("idRoomType")));
+					rt.setDescription((rs.getString("description")));
+					rt.setSizeRow((rs.getInt("rowQuantity")));
+					rt.setSizeCol((rs.getInt("colQuantity")));
+					
+					tipos.add(rt);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				if(rs!=null) {rs.close();}
+				if(stmt!=null) {stmt.close();}
+				DbConnector.getInstancia().releaseConn();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return tipos;
+		
+	} // getAvailablesByMovie
+	
+	
+	
 	public void delete(RoomType rt) {
 		PreparedStatement stmt=null;
 		try {
@@ -56,6 +112,8 @@ public class DataRoomType {
 					}
 			}
 	}
+	
+	
 
 	public RoomType createOne(RoomType rt) {
 		PreparedStatement stmt=null;
--- src/logic/RoomTypeController.java
+++ src/logic/RoomTypeController.java
@@ -1,12 +1,21 @@
 package logic;
 
+import java.util.LinkedList;
+
 import data.DataRoomType;
+import entities.Movie;
 import entities.RoomType;
 public class RoomTypeController {
+	
 	private DataRoomType drt;
 	
 	public RoomTypeController() {drt= new DataRoomType();}
 	public RoomType getOne(RoomType rt) {return drt.getOne(rt);}
+	
+	public LinkedList<RoomType>getAvailables(Movie m){
+		return drt.getAvailablesByMovie(m);
+	}
+	
 	public void deleteOne(RoomType rt) {drt.delete(rt);}
 	//public RoomType editOne(RoomType rt) {return drt.update(rt);}
 	public RoomType addOne(RoomType rt) {return drt.createOne(rt);}
