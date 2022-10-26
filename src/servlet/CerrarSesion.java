eec93440b160b6da7ceb286b2a62d4616bedae2f
flechaVivas <facundovivas15@gmail.com>
2022-10-26 17:24:36 -0300

    log out servlet

---
+++ src/servlet/CerrarSesion.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CerrarSesion
+ */
+@WebServlet("/src/cerrarSesion")
+public class CerrarSesion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CerrarSesion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getSession().removeAttribute("usuario");
+		request.getSession().invalidate();
+		
+		response.sendRedirect("/TPI-Cine/views/pages/index.jsp");
+		
+		
+		
+		
+	}
+
+}
