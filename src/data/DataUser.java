48238b4455c36ad55387ef6a03e5b58cc233f34a
flechaVivas <facundovivas15@gmail.com>
2022-11-04 20:02:52 -0300

    error handling - register

--- src/data/DataUser.java
+++ src/data/DataUser.java
@@ -3,13 +3,9 @@ package data;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.LinkedList;
-
-import com.sun.javadoc.ThrowsTag;
-
 import entities.Role;
 import entities.User;
 import logic.RoleController;
